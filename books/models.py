4ca588ffbbcf20f65527a39eeea8a3f644c4b202
sivascode <smithreen@gmail.com>
2019-06-16 20:07:42 +0530

    Register Book and use admin

--- books/models.py
+++ books/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 class Book (models.Model):
     title=models.CharField(max_length=70)
-    author=models.CharField(max_length=150)
+    author=models.CharField(max_length=150,help_text="Use Pen Name not your real Name")
     review=models.TextField(blank=True,null=True)
     date_reviewed=models.DateTimeField(blank=True,null=True)
-    is_favourite=models.BooleanField(default=False)
+    is_favourite=models.BooleanField(default=False,verbose_name="Favourite?")
+
+    def __str__(self):
+        return self.title
