c8a0aa127d8120bbb9fef7747d599cf7f7078bcd
sivascode <smithreen@gmail.com>
2019-06-18 22:41:17 +0530

    models

--- books/models.py
+++ books/models.py
@@ -1,13 +1,22 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 
-class Book (models.Model):
-    title=models.CharField(max_length=70)
-    author=models.CharField(max_length=150,help_text="Use Pen Name not your real Name")
-    review=models.TextField(blank=True,null=True)
-    date_reviewed=models.DateTimeField(blank=True,null=True)
-    is_favourite=models.BooleanField(default=False,verbose_name="Favourite?")
+class Book(models.Model):
+	title = models.CharField(max_length=150)
+	authors = models.ManyToManyField('Author', related_name='books')
+	review = models.TextField(blank=True, null=True)
+	date_reviewed = models.DateTimeField(blank=True, null=True)
+	is_favourite = models.BooleanField(default=False, verbose_name="Favourite?")
+	
+	def __str__(self):
+		return self.title
+		
 
-    def __str__(self):
-        return self.title
+class Author(models.Model):
+	name = models.CharField(max_length=70, help_text="Use pen name, not real name!",
+							unique=True)
+							
+	def __str__(self):
+		return self.name
